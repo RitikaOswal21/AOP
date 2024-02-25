ed2bf86fc58e922dc497de36fda0e756da9c96b7
RitikaOswal21 <159769366+RitikaOswal21@users.noreply.github.com>
2024-02-25 18:45:02 +0530

    Add files via upload

---
+++ springboot_project0/src/main/java/com/javainuse/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.javainuse.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import com.javainuse.model.Employee;
+import com.javainuse.service.EmployeeService;
+
+@RestController
+public class EmployeeController {
+
+	@Autowired
+	private EmployeeService employeeService;
+
+	@RequestMapping("/add/employee")
+	public Employee addEmployee(@RequestParam("name") String name, @RequestParam("empId") String empId) {
+
+		return employeeService.createEmployee(name, empId);
+
+	}
+
+	@RequestMapping("/remove/employee")
+	public String removeEmployee( @RequestParam("empId") String empId) {
+
+		employeeService.deleteEmployee(empId);
+
+		return "Employee removed";
+	}
+
+}
